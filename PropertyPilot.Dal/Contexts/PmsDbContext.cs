351c25ecf2e1eeb34a6703bdeafe49272ef805ac
Abdullah Yaser <buudi.dev@gmail.com>
2024-09-20 19:55:10 +0800

    chore: initial setup

---
+++ PropertyPilot.Dal/Contexts/PmsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PropertyPilot.Dal.Contexts;
+
+public class PmsDbContext(IConfiguration configuration) : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // connects to postgres with the connection string from appsettings
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("pms"));
+    }
+
+
+}
