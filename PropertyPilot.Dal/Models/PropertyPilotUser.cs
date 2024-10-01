983be7cce7dc47ec06525b52d88f43f58082f9b1
Abdullah Yaser <buudi.dev@gmail.com>
2024-10-01 22:11:30 +0800

    feat: create PropertyPilotUser entity

---
+++ PropertyPilot.Dal/Models/PropertyPilotUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PropertyPilot.Dal.Models;
+
+[Table(nameof(PropertyPilotUser))]
+class PropertyPilotUser
+{
+    public static class UserRoles
+    {
+        public static string AdminManager = nameof(AdminManager);
+        public static string Manager = nameof(Manager);
+        public static string Caretaker = nameof(Caretaker);
+    }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = string.Empty;
+
+    [Required]
+    public string HashedPassword { get; set; } = string.Empty;
+
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime CreatedOn { get; set; }
+
+    [Required]
+    [DefaultValue(false)]
+    public bool IsArchived { get; set; }
+
+    public DateTime? DateArchived { get; set; } = null;
+}
