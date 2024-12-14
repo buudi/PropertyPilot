ea6eff8057e31f1bec8d52d1abf844057a5a64f6
Abdullah Yaser <buudi.dev@gmail.com>
2024-12-14 20:03:18 +0800

    feat: contracts controller, endpoints get all and get by id

---
+++ PropertyPilot.Api/Controllers/ContractsController/ContractsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PropertyPilot.Dal.Models;
+using PropertyPilot.Services.ContractsServices;
+
+namespace PropertyPilot.Api.Controllers.ContractsController;
+
+/// <summary>
+/// 
+/// </summary>
+[Route("api/contracts")]
+[ApiController]
+public class ContractsController(ContractsService contractsService) : ControllerBase
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<List<Contract>> GetAllContracts()
+    {
+        var contracts = await contractsService.GetAllContracts();
+        return contracts;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<Contract>> GetContractById(Guid id)
+    {
+        var contract = await contractsService.GetContractById(id);
+
+        if (contract == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(contract);
+    }
+}
