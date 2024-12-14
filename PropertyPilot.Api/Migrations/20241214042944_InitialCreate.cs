b7d26efbd84a268e6af90d23a16f57a1d5366320
Abdullah Yaser <buudi.dev@gmail.com>
2024-12-14 14:14:06 +0800

    feat: properties, tenants, contracts Migration

---
+++ PropertyPilot.Api/Migrations/20241214042944_InitialCreate.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyPilot.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "properties",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    property_name = table.Column<string>(type: "text", nullable: false),
+                    emirate = table.Column<string>(type: "text", nullable: true),
+                    property_type = table.Column<string>(type: "text", nullable: false),
+                    units_count = table.Column<int>(type: "integer", nullable: true),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    is_archived = table.Column<bool>(type: "boolean", nullable: false),
+                    date_archived = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_properties", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contracts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tenant_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    property_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    rent = table.Column<double>(type: "double precision", nullable: false),
+                    notes = table.Column<string>(type: "text", nullable: true),
+                    active = table.Column<bool>(type: "boolean", nullable: false),
+                    renewable = table.Column<bool>(type: "boolean", nullable: false),
+                    move_out = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_contracts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_contracts_properties_property_id",
+                        column: x => x.property_id,
+                        principalTable: "properties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tenants",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    emirates_id = table.Column<string>(type: "text", nullable: true),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    current_contract_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    lifecycle_status = table.Column<string>(type: "text", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    date_archived = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_tenants", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_tenants_contracts_current_contract_id",
+                        column: x => x.current_contract_id,
+                        principalTable: "contracts",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_contracts_property_id",
+                table: "contracts",
+                column: "property_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_contracts_tenant_id",
+                table: "contracts",
+                column: "tenant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_tenants_current_contract_id",
+                table: "tenants",
+                column: "current_contract_id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_contracts_tenants_tenant_id",
+                table: "contracts",
+                column: "tenant_id",
+                principalTable: "tenants",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_contracts_properties_property_id",
+                table: "contracts");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_contracts_tenants_tenant_id",
+                table: "contracts");
+
+            migrationBuilder.DropTable(
+                name: "properties");
+
+            migrationBuilder.DropTable(
+                name: "tenants");
+
+            migrationBuilder.DropTable(
+                name: "contracts");
+        }
+    }
+}
