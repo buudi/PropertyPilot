357a70f5e7795483de3313ee9d69c1be991245ac
Abdullah Yaser <buudi.dev@gmail.com>
2024-12-16 00:08:08 +0800

    chore: bring the deleted changes back to Program.cs

--- PropertyPilot.Api/Program.cs
+++ PropertyPilot.Api/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PropertyPilot.Api.Extensions;
 using PropertyPilot.Dal.Contexts;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +14,90 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PropertyPilot API", Version = "v1" });
 
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+    // Configure Swagger to use JWT Authentication
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        //{securityScheme, new string[] { }}
+        { securityScheme, new[] { "Bearer" } }
+    });
+});
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_myAllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<PmsDbContext>();
+builder.Services.AddDbContext<PpDbContext>();
 builder.Services.AddPropertyPilotServices();
 
-builder.Services.AddDbContext<PpDbContext>();
+// Add JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+        NameClaimType = ClaimTypes.Email,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+// Add Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminManagerOnly", policy => policy.RequireRole
+            (PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.AdminManager));
+    options.AddPolicy("ManagerAndAbove", policy => policy.RequireRole
+            (PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.AdminManager,
+            PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.Manager));
+    options.AddPolicy("AllRoles", policy => policy.RequireRole
+            (PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.AdminManager,
+            PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.Manager,
+            PropertyPilot.Dal.Models.PropertyPilotUser.UserRoles.Caretaker));
+});
+
 
 var app = builder.Build();
 
@@ -20,11 +105,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PropertyPilot API v1"));
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
