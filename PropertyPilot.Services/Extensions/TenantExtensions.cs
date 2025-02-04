318e6a03dd96a842033b2ac667dc76e97dee70a1
bitlab-abdullah <abdullah.abdo@thebitlab.co>
2025-02-04 09:07:33 +0800

    feat: tenant extension to convert Tenant DAL Model into TenantListingRecord

---
+++ PropertyPilot.Services/Extensions/TenantExtensions.cs
@@ -0,0 +1,21 @@
+﻿using PropertyPilot.Dal.Models;
+using PropertyPilot.Services.TenantServices.Models;
+
+namespace PropertyPilot.Services.Extensions;
+
+public static class TenantExtensions
+{
+    public static TenantListingRecord AsTenantListingRecord(this Tenant tenant)
+    {
+        return new TenantListingRecord
+        {
+            Id = tenant.Id,
+            Name = tenant.Name,
+            PhoneNumber = tenant.PhoneNumber,
+            Email = tenant.Email,
+            TenantIdentification = tenant.TenantIdentification,
+            IsLeavingThisMonth = tenant.IsLeavingThisMonth,
+            IsAccountActive = tenant.IsAccountActive
+        };
+    }
+}
