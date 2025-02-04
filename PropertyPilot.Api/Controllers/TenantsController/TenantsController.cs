ea094c7d099efe035d96a9d0c43555035d14e0a8
bitlab-abdullah <abdullah.abdo@thebitlab.co>
2025-02-04 09:08:03 +0800

    feat: Tenant Controller, set up endpoints: create tenant, get all tenants, get tenant by Id

---
+++ PropertyPilot.Api/Controllers/TenantsController/TenantsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PropertyPilot.Services.TenantServices;
+using PropertyPilot.Services.TenantServices.Models;
+
+namespace PropertyPilot.Api.Controllers.TenantsController;
+
+/// <summary>
+/// Tenants Controller
+/// </summary>
+
+[Authorize(Policy = "ManagerAndAbove")]
+[Route("api/tenants")]
+[ApiController]
+public class TenantsController(TenantService tenantService) : ControllerBase
+{
+
+    /// <summary>
+    /// get all tenants listing
+    /// </summary>
+    /// <returns>list of tenant records</returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllTenants([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+    {
+        var tenants = await tenantService.GetAllTenantsListingAsync(pageNumber, pageSize);
+
+        return Ok(tenants);
+    }
+
+    /// <summary>
+    /// Get Tenant Listing Record By Tenant Id
+    /// </summary>
+    /// <param name="tenantId">Tenant UUID</param>
+    /// <returns>Tenant Listing Record</returns>
+    [HttpGet("{tenantId:guid}")]
+    public async Task<IActionResult> GetTenantById(Guid tenantId)
+    {
+        var tenant = await tenantService.GetTenantRecordAsync(tenantId);
+
+        if (tenant == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tenant);
+    }
+
+    /// <summary>
+    /// Create Tenant id
+    /// </summary>
+    /// <param name="tenantCreateRequest"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> CreateTenant(TenantCreateRequest tenantCreateRequest)
+    {
+        var newTenant = await tenantService.CreateTenantAsync(tenantCreateRequest);
+        return CreatedAtAction(nameof(GetTenantById), new { tenantId = newTenant.Id }, newTenant);
+    }
+}
