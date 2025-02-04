ece256011c93158929a5b35e88abfa695773a28f
bitlab-abdullah <abdullah.abdo@thebitlab.co>
2025-02-04 09:06:26 +0800

    feat: tenant create request model

---
+++ PropertyPilot.Services/TenantServices/Models/TenantCreateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PropertyPilot.Services.TenantServices.Models;
+
+public record TenantCreateRequest
+{
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    public required string PhoneNumber { get; set; }
+
+    [Required]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string TenantIdentification { get; set; }
+}
