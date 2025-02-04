f6a52331a146449b376b3c4af72b233b6f76ba6d
bitlab-abdullah <abdullah.abdo@thebitlab.co>
2025-02-04 09:06:13 +0800

    feat: tenant listing record model

---
+++ PropertyPilot.Services/TenantServices/Models/TenantListingRecord.cs
@@ -0,0 +1,14 @@
+﻿using PropertyPilot.Dal.Abstractions;
+
+namespace PropertyPilot.Services.TenantServices.Models;
+
+public record TenantListingRecord : ITenant
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public required string TenantIdentification { get; set; }
+    public required bool IsLeavingThisMonth { get; set; }
+    public required bool IsAccountActive { get; set; }
+}
