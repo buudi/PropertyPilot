b0adcd8d6fb828f416a5981f548ba2153f8a6b1c
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-07 01:59:27 +0800

    feat: add to the tenant create request object to match the Frontend Request body

--- PropertyPilot.Services/TenantServices/Models/TenantCreateRequest.cs
+++ PropertyPilot.Services/TenantServices/Models/TenantCreateRequest.cs
@@ -15,4 +15,25 @@ public record TenantCreateRequest
 
     [Required]
     public required string TenantIdentification { get; set; }
+
+    [Required]
+    public required bool IsAccountActive { get; set; }
+
+    [Required]
+    public required DateTime TenancyStart { get; set; }
+
+    [Required]
+    public required bool IsInvoiceRenewable { get; set; }
+
+    public DateTime? TenancyEnd { get; set; }
+
+    [Required]
+    public required Guid PropertyUnitId { get; set; }
+
+    public Guid SubUnitId { get; set; } = Guid.Empty; // optional
+
+    [Required]
+    public required double AssignedRent { get; set; }
+
+    public double? OneTimeDiscount { get; set; }
 }
