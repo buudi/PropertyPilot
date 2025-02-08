a7af9d9a714eac1ec86e035bc0bfbf1d6abb7f26
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-08 19:23:35 +0800

    feat: InvoiceRecord Model

---
+++ PropertyPilot.Services/InvoiceServices/Models/InvoiceRecord.cs
@@ -0,0 +1,9 @@
+﻿using PropertyPilot.Dal.Models;
+
+namespace PropertyPilot.Services.InvoiceServices.Models;
+
+public record InvoiceRecord
+{
+    public required Invoice Invoice { get; init; }
+    public required List<InvoiceItem> InvoiceItems { get; init; } = [];
+}
