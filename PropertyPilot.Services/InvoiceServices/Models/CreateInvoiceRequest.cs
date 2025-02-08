15df0c9e2d4dae044ad32bbc7bb9c01f9a53a66a
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-08 19:23:50 +0800

    feat: CreateInvoiceRequestModel

---
+++ PropertyPilot.Services/InvoiceServices/Models/CreateInvoiceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyPilot.Services.InvoiceServices.Models;
+
+public class CreateInvoiceRequest
+{
+    public double? Discount { get; set; }
+    public required Guid TenantId { get; set; }
+    public required DateTime DateStart { get; set; }
+    public DateTime? DateDue { get; set; }
+    public required bool IsRenewable { get; set; }
+    public required string InvoiceStatus { get; set; }
+}
