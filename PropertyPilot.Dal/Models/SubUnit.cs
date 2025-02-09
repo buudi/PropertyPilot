13348d0a4a78f7d6e93d22a69e2d117dc1c367c3
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-09 19:56:25 +0800

    feat: add SubUnit DAL Model

---
+++ PropertyPilot.Dal/Models/SubUnit.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PropertyPilot.Dal.Models;
+
+[Table((nameof(SubUnit)))]
+public class SubUnit
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public required Guid PropertyListingId { get; set; } // FK to PropertyListing table
+    public required string IdentifierName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
