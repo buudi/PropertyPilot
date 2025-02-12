00a1788a395e8063c162504016b64b94051ec97e
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-13 02:49:17 +0800

    feat: RentPayment extension, AsRentPaymentRecord

---
+++ PropertyPilot.Services/Extensions/RentPaymentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyPilot.Dal.Contexts;
+using PropertyPilot.Dal.Models;
+using PropertyPilot.Services.FinanceServices.Models;
+
+namespace PropertyPilot.Services.Extensions;
+
+public static class RentPaymentExtensions
+{
+    public static async Task<RentPaymentTransactionRecord> AsRentPaymentRecord(this RentPayment rentPayment, PmsDbContext pmsDbContext)
+    {
+        var transaction = await pmsDbContext.Transactions
+            .Where(x => x.TransactionType == Transaction.TransactionTypes.RentPayment)
+            .Where(x => x.ReferenceId == rentPayment.Id)
+            .FirstOrDefaultAsync();
+
+        return new RentPaymentTransactionRecord { RentPayment = rentPayment, Transaction = transaction! };
+    }
+}
