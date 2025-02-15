602e83e7616ede14d18dc3e61e070f0de502da0c
Abdullah Yaser <buudi.dev@gmail.com>
2025-02-15 20:57:44 +0800

    feat: expenses extension method to return  as ExpenseTransactionRecord

---
+++ PropertyPilot.Services/Extensions/ExpenseExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyPilot.Dal.Contexts;
+using PropertyPilot.Dal.Models;
+using PropertyPilot.Services.FinanceServices.Models;
+
+namespace PropertyPilot.Services.Extensions;
+
+public static class ExpenseExtensions
+{
+    public static async Task<ExpenseTransactionRecord> AsExpenseTransactionRecord(this Expense expense,
+        PmsDbContext pmsDbContext)
+    {
+        var transaction = await pmsDbContext.Transactions
+            .Where(x => x.TransactionType == Transaction.TransactionTypes.Expense)
+            .Where(x => x.ReferenceId == expense.Id)
+            .FirstOrDefaultAsync();
+
+        return new ExpenseTransactionRecord
+        {
+            Expense = expense,
+            Transaction = transaction!
+        };
+    }
+}
---
+++ PropertyPilot.Services/FinanceServices/Models/ExpenseTransactionRecord.cs
@@ -0,0 +1,9 @@
+﻿using PropertyPilot.Dal.Models;
+
+namespace PropertyPilot.Services.FinanceServices.Models;
+
+public record ExpenseTransactionRecord
+{
+    public Expense Expense { get; set; }
+    public Transaction Transaction { get; set; }
+}
