aef7816cf1e3c47afbc57fb66d6ba750a8e436be
Abdullah Yaser <buudi.dev@gmail.com>
2025-04-26 23:16:05 +0800

    feat: http extensions to get user Id

---
+++ PropertyPilot.Api/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+
+namespace PropertyPilot.Api.Extensions;
+
+/// <summary>
+/// Extensions for HttpContext.
+/// </summary>
+public static class HttpContextExtensions
+{
+    /// <summary>
+    /// Get the user Guid from the HttpContext.
+    /// </summary>
+    /// <param name="ctx"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static Guid GetUserId(this HttpContext ctx)
+    {
+        if (ctx?.User == null)
+            throw new ArgumentNullException(nameof(ctx), "HttpContext or User is null.");
+
+        //extract the user Guid from the ClaimsPrincipal    
+        var userIdClaim = ctx.User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            throw new InvalidOperationException("User ID claim not found.");
+
+        return Guid.Parse(userIdClaim.Value);
+    }
+}
