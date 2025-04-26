6d99e2a879184214781d6382aff9417d24d32e9e
Abdullah Yaser <buudi.dev@gmail.com>
2025-04-26 23:37:11 +0800

    chore: temporarily allow all origins, CORS policy

--- PropertyPilot.Api/Program.cs
+++ PropertyPilot.Api/Program.cs
@@ -47,19 +47,30 @@ builder.Services.AddSwaggerGen(c =>
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+// temporarily allow all origins
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "_myAllowSpecificOrigins", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-
-        policy.WithOrigins("http://localhost:5174")
+        policy.AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: "_myAllowSpecificOrigins", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:5173")
+//            .AllowAnyHeader()
+//            .AllowAnyMethod();
+
+//        policy.WithOrigins("http://localhost:5174")
+//            .AllowAnyHeader()
+//            .AllowAnyMethod();
+//    });
+//});
 
 builder.Services.AddDbContext<PmsDbContext>();
 builder.Services.AddDbContext<PpDbContext>();
